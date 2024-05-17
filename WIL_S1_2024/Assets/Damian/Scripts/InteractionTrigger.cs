6f22990feca80b80497cd8da06e97de223ac2469
Damian Grobler <54507680+Rikiflame01@users.noreply.github.com>
2024-05-17 10:58:55 +0200

    Interaction Prompts

---
+++ WIL_S1_2024/Assets/Damian/Scripts/InteractionTrigger.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class InteractionTrigger : MonoBehaviour
+{
+    public GameObject interactionCanvasPrefab;
+    public Transform canvasSpawnPoint;
+    private GameObject instantiatedCanvas;
+    public Camera mainCamera;
+
+    void Start()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+    }
+
+    void Update()
+    {
+        if (instantiatedCanvas != null)
+        {
+            instantiatedCanvas.transform.LookAt(mainCamera.transform);
+            instantiatedCanvas.transform.Rotate(0, 180, 0); 
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            if (instantiatedCanvas == null && canvasSpawnPoint != null)
+            {
+                instantiatedCanvas = Instantiate(interactionCanvasPrefab, canvasSpawnPoint.position, Quaternion.identity);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (instantiatedCanvas != null)
+            {
+                Destroy(instantiatedCanvas);
+            }
+        }
+    }
+}
