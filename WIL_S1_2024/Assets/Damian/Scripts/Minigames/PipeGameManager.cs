faee4f6cbe0aea5aa1ed26508c2c8b312d9a55cd
Damian Grobler <54507680+Rikiflame01@users.noreply.github.com>
2024-05-23 10:25:24 +0200

    Pipe minigame update
    Lerping and button rotation instead of sprite rotation

--- WIL_S1_2024/Assets/Damian/Scripts/Minigames/PipeGameManager.cs
+++ WIL_S1_2024/Assets/Damian/Scripts/Minigames/PipeGameManager.cs
@@ -1,38 +1,72 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections;
 
 public class PipeGameManager : MonoBehaviour
 {
+    [Tooltip("Button representing the start of the pipe route.")]
     public Button startButton;
+
+    [Tooltip("Button representing the end of the pipe route.")]
     public Button endButton;
+
+    [Tooltip("Array of buttons representing the pipes.")]
     public Button[] pipeButtons;
+
+    [Tooltip("Panel that displays the completion message.")]
     public GameObject messagePanel;
+
+    [Tooltip("Duration for the smooth rotation of the pipes.")]
+    public float rotationDuration = 0.5f;
 
     private int[] pipeStates;
     private int totalPipes;
 
     void Start()
     {
+        InitializeGame();
+    }
+
+    private void InitializeGame()
+    {
         totalPipes = pipeButtons.Length;
         pipeStates = new int[totalPipes];
+
         for (int i = 0; i < totalPipes; i++)
         {
             int index = i;
             pipeButtons[i].onClick.AddListener(() => RotatePipe(index));
         }
+
         messagePanel.SetActive(false);
         GenerateMap();
     }
 
-    void RotatePipe(int index)
+    private void RotatePipe(int index)
     {
         pipeStates[index] = (pipeStates[index] + 1) % 4;
-        pipeButtons[index].transform.Rotate(0, 0, 90);
+        StartCoroutine(SmoothRotate(pipeButtons[index].transform, 90));
         CheckPipesAlignment();
     }
 
-    void GenerateMap()
+    private IEnumerator SmoothRotate(Transform target, float angle)
+    {
+        Quaternion startRotation = target.rotation;
+        Quaternion endRotation = startRotation * Quaternion.Euler(0, 0, angle);
+        float elapsedTime = 0;
+
+        while (elapsedTime < rotationDuration)
+        {
+            target.rotation = Quaternion.Slerp(startRotation, endRotation, elapsedTime / rotationDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        target.rotation = endRotation;
+    }
+
+    private void GenerateMap()
     {
         RectTransform startRect = startButton.GetComponent<RectTransform>();
         RectTransform endRect = endButton.GetComponent<RectTransform>();
@@ -45,37 +79,32 @@ public class PipeGameManager : MonoBehaviour
         float rightEdge = panelWidth / 2 - buttonWidth / 2;
         float spacing = (rightEdge - leftEdge) / (totalPipes + 1);
 
-        Debug.Log("Panel Width: " + panelWidth);
-        Debug.Log("Button Width: " + buttonWidth);
-        Debug.Log("Spacing: " + spacing);
-
-        startRect.anchoredPosition = new Vector2(leftEdge, 0);
-        endRect.anchoredPosition = new Vector2(rightEdge, 0);
-
-        Debug.Log("Start Button Position: " + startRect.anchoredPosition);
-        Debug.Log("End Button Position: " + endRect.anchoredPosition);
+        PositionButton(startRect, leftEdge, "Start Button");
+        PositionButton(endRect, rightEdge, "End Button");
 
         for (int i = 0; i < totalPipes; i++)
         {
             RectTransform pipeRect = pipeButtons[i].GetComponent<RectTransform>();
             float xPos = leftEdge + (i + 1) * spacing;
             pipeRect.anchoredPosition = new Vector2(xPos, 0);
+            Debug.Log($"Pipe Button {i} Position: {pipeRect.anchoredPosition}");
 
-            Debug.Log("Pipe Button " + i + " Position: " + pipeRect.anchoredPosition);
-        }
-
-        for (int i = 0; i < totalPipes; i++)
-        {
             pipeStates[i] = Random.Range(1, 4);
             pipeButtons[i].transform.rotation = Quaternion.Euler(0, 0, 90 * pipeStates[i]);
         }
     }
 
-    void CheckPipesAlignment()
+    private void PositionButton(RectTransform rect, float position, string buttonName)
+    {
+        rect.anchoredPosition = new Vector2(position, 0);
+        Debug.Log($"{buttonName} Position: {rect.anchoredPosition}");
+    }
+
+    private void CheckPipesAlignment()
     {
         foreach (int state in pipeStates)
         {
-            if (state != 0) 
+            if (state != 0)
             {
                 return;
             }
