6864d736dc2b97c454b17aa4c420a500880f04b0
KeganWB <Keganb24@gmail.com>
2024-11-19 16:01:43 +0200

    Setup Done
    Need to add assets

--- WIL_S1_2024/Assets/Damian/Scripts/Minigames/generator MiniGame/GeneratorMiniGameManager.cs
+++ WIL_S1_2024/Assets/Damian/Scripts/Minigames/generator MiniGame/GeneratorMiniGameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class GeneratorMiniGameManager : MonoBehaviour
@@ -11,28 +12,30 @@ public class GeneratorMiniGameManager : MonoBehaviour
     [Tooltip("Panel to display upon completion.")]
     public GameObject completionPanel;
 
+    [Tooltip("Main generator canvas that will be deactivated after completion.")]
+    public Canvas generatorMiniGameCanvas; 
+
     private void OnEnable()
     {
-        //Reset game state whenever the canvas is enabled
         ResetGame();
     }
 
     private void ResetGame()
     {
-        completionPanel.SetActive(false);
+        completionPanel.SetActive(false); //Hide the completion panel initially
 
         foreach (GameObject wire in wires)
         {
             var wireConnector = wire.GetComponent<GeneratorMiniGameLogic>();
-            wireConnector.enabled = true;
-
-            //Reset position to original state
+            wireConnector.enabled = true; 
+            
             wire.GetComponent<RectTransform>().anchoredPosition = wireConnector.GetComponent<RectTransform>().anchoredPosition;
         }
     }
 
     public void CheckCompletion()
     {
+        //Check if all wires are connected
         foreach (GameObject wire in wires)
         {
             if (wire.GetComponent<GeneratorMiniGameLogic>().enabled)
@@ -41,8 +44,32 @@ public class GeneratorMiniGameManager : MonoBehaviour
             }
         }
 
-        //All wires connected
+        //All wires are connected, show the completion panel
         completionPanel.SetActive(true);
         Debug.Log("Mini-game completed!");
+
+        //Add 100 electricity upon completion
+        IdleResources.Instance.AddResource(BuildingData.ResourceType.Electricity, 100);
+
+        
+        StartCoroutine(HideCanvasAfterDelay());
+    }
+
+    private IEnumerator HideCanvasAfterDelay()
+    {
+        yield return new WaitForSeconds(3f);
+
+        //Hide the entire generator canvas
+        if (generatorMiniGameCanvas != null)
+        {
+            generatorMiniGameCanvas.gameObject.SetActive(false);
+            Debug.Log("Generator Mini-Game canvas hidden.");
+        }
+        else
+        {
+            Debug.LogError("GeneratorMiniGameCanvas is not assigned!");
+        }
+
+        Debug.Log("Generator Mini-Game completed and entire canvas hidden.");
     }
 }
