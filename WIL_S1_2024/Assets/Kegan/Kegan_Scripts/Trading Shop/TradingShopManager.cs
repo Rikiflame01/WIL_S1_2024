d10ddf6ef2c4253b6350d656aa98d06b88ea564e
KeganWB <Keganb24@gmail.com>
2024-11-20 23:35:51 +0200

    Shop Manager Setup

---
+++ WIL_S1_2024/Assets/Kegan/Kegan_Scripts/Trading Shop/TradingShopManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class TradingShopManager : MonoBehaviour
+{
+    [Tooltip("Reference to the IdleResources script.")]
+    public IdleResources idleResources;
+
+    //Trades 20 Rand for 20 Water and 20 Electricity
+    public void TradeRandForResources()
+    {
+        if (idleResources.rand >= 20)
+        {
+            idleResources.SpendResource(BuildingData.ResourceType.Rand, 20);
+            idleResources.AddResource(BuildingData.ResourceType.Water, 20);
+            idleResources.AddResource(BuildingData.ResourceType.Electricity, 20);
+            Debug.Log("Traded 20 Rand for 20 Water and 20 Electricity.");
+        }
+        else
+        {
+            Debug.Log("Not enough Rand to trade.");
+        }
+    }
+
+    //Trades 20 Electricity and 20 Water for 40 Rand
+    public void TradeResourcesForRand()
+    {
+        if (idleResources.electricity >= 20 && idleResources.water >= 20)
+        {
+            idleResources.SpendResource(BuildingData.ResourceType.Electricity, 20);
+            idleResources.SpendResource(BuildingData.ResourceType.Water, 20);
+            idleResources.AddResource(BuildingData.ResourceType.Rand, 40);
+            Debug.Log("Traded 20 Electricity and 20 Water for 40 Rand.");
+        }
+        else
+        {
+            Debug.Log("Not enough resources to trade for Rand.");
+        }
+    }
+}
