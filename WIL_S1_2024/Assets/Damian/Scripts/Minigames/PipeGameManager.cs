1060956761c4107962603a3bef29132b0dfedd44
Damian Grobler <54507680+Rikiflame01@users.noreply.github.com>
2024-11-21 15:38:59 +0200

    PipeGame

--- WIL_S1_2024/Assets/Damian/Scripts/Minigames/PipeGameManager.cs
+++ WIL_S1_2024/Assets/Damian/Scripts/Minigames/PipeGameManager.cs
@@ -5,12 +5,8 @@ using System.Collections.Generic;
 
 public class PipeGameManager : MonoBehaviour
 {
-
-    [Tooltip("Button representing the start of the pipe route.")]
-    public Button startButton;
-
-    [Tooltip("Button representing the end of the pipe route.")]
-    public Button endButton;
+    [Tooltip("Parent canvas for the game.")]
+    public Canvas parentCanvas;
 
     [Tooltip("Array of buttons representing the pipes.")]
     public Button[] pipeButtons;
@@ -18,119 +14,93 @@ public class PipeGameManager : MonoBehaviour
     [Tooltip("Panel that displays the completion message.")]
     public GameObject messagePanel;
 
-    [Tooltip("Duration for the smooth rotation of the pipes.")]
-    public float rotationDuration = 0.5f;
-
-    private int[] pipeStates;
-    private int totalPipes;
-    private Dictionary<int, Coroutine> activeCoroutines = new Dictionary<int, Coroutine>();
-    private Dictionary<int, Quaternion> targetRotations = new Dictionary<int, Quaternion>();
-    private Canvas parentCanvas;
+    private int rotationLimit = 10;
+    private HashSet<int> rotatedIndexes;
+    private float[] originalZRotations;
 
     void Start()
     {
         InitializeGame();
-        parentCanvas = GetComponentInParent<Canvas>();
     }
 
     private void InitializeGame()
     {
-        totalPipes = pipeButtons.Length;
-        pipeStates = new int[totalPipes];
+        originalZRotations = new float[pipeButtons.Length];
 
-        for (int i = 0; i < totalPipes; i++)
+        for (int i = 0; i < pipeButtons.Length; i++)
         {
             int index = i;
+            originalZRotations[i] = Mathf.Round(pipeButtons[i].transform.localEulerAngles.z);
             pipeButtons[i].onClick.AddListener(() => RotatePipe(index));
-            targetRotations[i] = pipeButtons[i].transform.rotation;
         }
 
         messagePanel.SetActive(false);
-        GenerateMap();
+
+        ApplyRandomRotations();
     }
 
     private void RotatePipe(int index)
     {
-        pipeStates[index] = (pipeStates[index] + 1) % 4;
-        targetRotations[index] *= Quaternion.Euler(0, 0, 90);
+        pipeButtons[index].transform.Rotate(0, 0, 90);
 
-        if (activeCoroutines.ContainsKey(index))
-        {
-            StopCoroutine(activeCoroutines[index]);
-        }
-
-        activeCoroutines[index] = StartCoroutine(SmoothRotate(pipeButtons[index].transform, targetRotations[index]));
         CheckPipesAlignment();
     }
 
-    private IEnumerator SmoothRotate(Transform target, Quaternion targetRotation)
+    private void ApplyRandomRotations()
     {
-        Quaternion startRotation = target.rotation;
-        float elapsedTime = 0;
-
-        while (elapsedTime < rotationDuration)
+        rotatedIndexes = new HashSet<int>();
+        while (rotatedIndexes.Count < rotationLimit && rotatedIndexes.Count < pipeButtons.Length)
         {
-            target.rotation = Quaternion.Slerp(startRotation, targetRotation, elapsedTime / rotationDuration);
-            elapsedTime += Time.deltaTime;
-            yield return null;
+            int randomIndex = Random.Range(0, pipeButtons.Length);
+            rotatedIndexes.Add(randomIndex);
         }
 
-        target.rotation = targetRotation;
-    }
-
-    private void GenerateMap()
-    {
-        RectTransform startRect = startButton.GetComponent<RectTransform>();
-        RectTransform endRect = endButton.GetComponent<RectTransform>();
-        RectTransform panelRect = GetComponent<RectTransform>();
-
-        float panelWidth = panelRect.rect.width;
-        float buttonWidth = pipeButtons[0].GetComponent<RectTransform>().rect.width;
-
-        float leftEdge = -panelWidth / 2 + buttonWidth / 2;
-        float rightEdge = panelWidth / 2 - buttonWidth / 2;
-        float spacing = (rightEdge - leftEdge) / (totalPipes + 1);
-
-        PositionButton(startRect, leftEdge, "Start Button");
-        PositionButton(endRect, rightEdge, "End Button");
-
-        for (int i = 0; i < totalPipes; i++)
+        foreach (int index in rotatedIndexes)
         {
-            RectTransform pipeRect = pipeButtons[i].GetComponent<RectTransform>();
-            float xPos = leftEdge + (i + 1) * spacing;
-            pipeRect.anchoredPosition = new Vector2(xPos, 0);
-            Debug.Log($"Pipe Button {i} Position: {pipeRect.anchoredPosition}");
+            int randomRotationSteps = Random.Range(1, 4);
+            pipeButtons[index].transform.Rotate(0, 0, 90 * randomRotationSteps);
 
-            pipeStates[i] = Random.Range(1, 4);
-            pipeButtons[i].transform.rotation = Quaternion.Euler(0, 0, 90 * pipeStates[i]);
-            targetRotations[i] = pipeButtons[i].transform.rotation;
+            Vector3 currentEulerAngles = pipeButtons[index].transform.localEulerAngles;
+            currentEulerAngles.z = Mathf.Round(NormalizeAngle(currentEulerAngles.z));
+            pipeButtons[index].transform.localEulerAngles = currentEulerAngles;
         }
-    }
-
-    private void PositionButton(RectTransform rect, float position, string buttonName)
-    {
-        rect.anchoredPosition = new Vector2(position, 0);
-        Debug.Log($"{buttonName} Position: {rect.anchoredPosition}");
     }
 
     private void CheckPipesAlignment()
     {
-        foreach (int state in pipeStates)
+        int correctCount = 0;
+
+        for (int i = 0; i < pipeButtons.Length; i++)
         {
-            if (state != 0)
+            float currentZ = Mathf.Round(NormalizeAngle(pipeButtons[i].transform.localEulerAngles.z));
+            float originalZ = Mathf.Round(NormalizeAngle(originalZRotations[i]));
+
+            if (Mathf.Approximately(currentZ, originalZ))
             {
-                return;
+                correctCount++;
             }
         }
-        messagePanel.SetActive(true);
-        StartCoroutine(ShowMessagePanel());
+
+        if (correctCount == pipeButtons.Length)
+        {
+            StartCoroutine(ShowMessagePanel());
+        }
     }
 
     private IEnumerator ShowMessagePanel()
     {
+        messagePanel.SetActive(true);
         IdleResources.Instance.water += 100;
         yield return new WaitForSeconds(5f);
         messagePanel.SetActive(false);
+        ApplyRandomRotations();
         parentCanvas.gameObject.SetActive(false);
+    }
+
+    private float NormalizeAngle(float angle)
+    {
+        while (angle < 0) angle += 360;
+        while (angle >= 360) angle -= 360;
+        return angle;
     }
 }
